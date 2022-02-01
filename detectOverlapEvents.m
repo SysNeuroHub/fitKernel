b74ef4974fa9d66b6d080f764176f1e9db1d24b0
Daisuke <sageagainstthemachine@gmail.com>
2022-02-01 18:10:21 +1100

    detectOverlapEvents
    very slow

---
+++ detectOverlapEvents.m
@@ -0,0 +1,23 @@
+function evOverlappedIdx = detectOverlapEvents(taxis, eventTimes, excEventTimes)
+%eventTimes = [events x 2]
+
+excTrace = event2Trace(taxis, excEventTimes);
+
+evOverlapped = zeros(length(eventTimes),1);
+for iev = 1:length(eventTimes)
+    thisEvTrace=event2Trace(taxis, [eventTimes(iev,1) eventTimes(iev,end)]);
+    evOverlapped(iev) = (thisEvTrace'*excTrace > 0);
+end
+evOverlappedIdx = find(evOverlapped);
+
+
+%
+% test = arrayfun(@(x)(event2Trace(t,x)), [drStartTimes_th drEndTimes_th], ...
+%     'UniformOutput', false);
+%cumsum(test{:,2}) - cumsum(test{:,1})
+%drEndTraces=cellfun(@(x)cumsum(x), test(:,2), 'UniformOutput',
+%false);outofmemory
+
+%test=num2cell( [drStartTimes_th drEndTimes_th], 2);
+%excMat = cellfun(@(x)(event2Trace(t,x)), test, 'UniformOutput', false);
+%excludeTraces = cell2mat(excMat);%too large
