ec01bc3291685758d5d1e3089799ef5e02a9fcc7
Daisuke Shimaoka <daisuke.shimaoka@monash.edu>
2023-06-21 12:30:20 +1000

    added description for retrieveTrIdx_r.m

--- retrieveTrIdx_r.m
+++ retrieveTrIdx_r.m
@@ -1,4 +1,12 @@
 function [trIdx_r, trIdx] = retrieveTrIdx_r(t_cat, t_cat_r, t_tr)
+%[trIdx_r, trIdx] = retrieveTrIdx_r(t_cat, t_cat_r, t_tr)
+%INPUT:
+%       t_cat: consecutive time
+%       t_r: consecutive time after resampling at param.dt_r
+%       t_tr: time index of each trial before resampling
+%OUTPUT:
+%       trIdx_r:
+%       trIdx:
 
 catidx = 1;
 trIdx = [];
